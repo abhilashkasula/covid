95bf94fc09094099fff1453feb624aa60f6497ce
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-05-18 20:20:08 +0530

    added districts data

--- js/index.js
+++ js/index.js
@@ -63,29 +63,67 @@ const getStateCount = function(districts) {
   }, {c:0, cd:0, a:0, ad:0, d:0, dd:0, r:0, rd:0})
 };
 
+const getDelta = (n) => n ? `↑${n}` : '';
+
 const generateStates = function(state) {
-  const getDelta = (n) => n ? `[+${n}]` : '';
   const {c, cd, a, ad, d, dd, r, rd} = getStateCount(state.districtData);
   return `
   <div class="state">
     <h2 class="state-name">${state.state}</h2>
     <table>
       <tr>
-        <td class="confirmed-text value"><span class="delta">${getDelta(cd)} </span>${c}</td>
+        <td class="value confirmed-text"><span class="delta">${getDelta(cd)}</span> ${c}</td>
       </tr>
       <tr>
-      <td class="active-text value"><span class="delta">${getDelta(ad)} </span>${a}</td>
+      <td class="value active-text"><span class="delta">${getDelta(ad)}</span> ${a}</td>
       </tr>
       <tr>
-      <td class="deceased-text value"><span class="delta">${getDelta(dd)} </span>${d}</td>
+      <td class="value deceased-text"><span class="delta">${getDelta(dd)}</span> ${d}</td>
       </tr>
       <tr>
-      <td class="recovered-text value"><span class="delta">${getDelta(rd)} </span>${r}</td>
+      <td class="value recovered-text"><span class="delta">${getDelta(rd)}</span> ${r}</td>
       </tr>
     </table>
   </div>
+  <div class="state-info hide" id="state-${state.state.replace(/ /g, '-')}"></div>
 `
-}
+};
+
+const generateDistrict = function(district, zones) {
+  const dist = zones.find(z => z.district ===district.district);
+  const zone = dist ? dist.zone : 'Unknown';
+  return `
+    <tr>
+    <td class="district ${zone}-zone">${district.district}</td>
+    <td class="col"><span class="confirmed-text">${getDelta(district.delta.confirmed)}</span> ${district.confirmed}</td>
+    <td class="col">${district.active}</td>
+    <td class="col"><span class="deceased-text">${getDelta(district.delta.deceased)}</span> ${district.deceased}</td>
+    <td class="col"><span class="recovered-text">${getDelta(district.delta.recovered)}</span> ${district.recovered}</td>
+    </tr>
+    `
+};
+
+const drawDistricts = function(data, zones, state) {
+  const states = Array.from(document.querySelectorAll('.state-info'));
+  states.forEach(state => state.classList.add('hide'));
+  const elem = document.querySelector(`#state-${state.replace(/ /g, '-')}`);
+  const districts = data.find(s => s.state === state).districtData;
+  let html = `
+  <table>
+    <thead>
+      <th class="deceased-text">State</th>
+      <th class="confirmed-text">Cnfrmd</th>
+      <th class="active-text">Active</th>
+      <th class="deceased-text">Dcsd</th>
+      <th class="recovered-text">Rcvrd</th>
+    </thead>
+    <tbody>`
+  html += districts.map(district => generateDistrict(district, zones)).join('');
+  html += '</tbody></table>';
+  console.log(elem)
+  elem.classList.remove('hide');
+  elem.innerHTML = html;
+};
 
 const sortOnConfirmedCases = (a, b) => {
   const {c: confirmedA} = getStateCount(a.districtData);
@@ -98,11 +136,18 @@ const drawStates = function(data) {
   document.querySelector('#states').innerHTML = html;
 };
 
-const addListeners = function(data) {
+const addListeners = function(data, zones) {
   Array.from(document.querySelectorAll('.state')).forEach(s => {
-    s.addEventListener('click', () => {
-      const name = event.target.innerText;
-      showDistricts(data, name);
+    s.addEventListener('click', (event) => {
+      let target = event.target;
+      if(event.target.classList[0] === 'value') {
+        target = event.target.parentElement.parentElement.parentElement.parentElement;
+      }
+      if(event.target.classList[0] === 'state-name') {
+        target = event.target.parentElement;
+      }
+      const name = target.children[0].innerText;
+      drawDistricts(data, zones, name);
     });
   });
 
@@ -116,16 +161,22 @@ const addListeners = function(data) {
   });
 };
 
-const drawInfo = function(data) {
+const drawInfo = function(data, zones) {
   drawTotalStats(data);
   drawStates(data);
-  addListeners(data);
+  addListeners(data, zones);
 };
+
+const fetchZones = function(data) {
+  fetch('https://api.covid19india.org/zones.json')
+    .then(res => res.json())
+    .then(zones => drawInfo(data, zones.zones));
+}
 
 const main = function () {
   fetch('https://api.covid19india.org/v2/state_district_wise.json')
     .then((res) => res.json())
-    .then(drawInfo);
+    .then(fetchZones);
 };
 
 window.onload = main;
